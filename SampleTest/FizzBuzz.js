938cd9404068da9b5833dc571e95e29107562733
marshall-strong <marshallstrong123@gmail.com>
2022-02-19 15:13:31 -0500

    `fizzBuzz` function passes all test cases.

--- SampleTest/FizzBuzz.js
+++ SampleTest/FizzBuzz.js
@@ -27,7 +27,21 @@ function readLine() {
  */
 
 function fizzBuzz(n) {
-  // Write your code here
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0) {
+      if (i % 5 === 0) {
+        console.log("FizzBuzz");
+      } else {
+        console.log("Fizz");
+      }
+    } else {
+      if (i % 5 === 0) {
+        console.log("Buzz");
+      } else {
+        console.log(i);
+      }
+    }
+  }
 }
 
 function main() {
