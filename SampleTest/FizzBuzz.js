dd1875016c4010d197cfceb3c6ea34379acd0167
marshall-strong <marshallstrong123@gmail.com>
2022-02-19 15:35:11 -0500

    add a "more readable" solution to `fizzBuzz`

--- SampleTest/FizzBuzz.js
+++ SampleTest/FizzBuzz.js
@@ -4,6 +4,7 @@
  * The function accepts INTEGER n as parameter.
  */
 
+// my solution
 function fizzBuzz(n) {
   for (let i = 1; i <= n; i++) {
     if (i % 3 === 0) {
@@ -19,5 +20,16 @@ function fizzBuzz(n) {
         console.log(i);
       }
     }
+  }
+}
+
+// https://flexiple.com/fizzbuzz-javascript/
+// most readable solution
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
   }
 }
