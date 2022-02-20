72166d71190cf0048d6d872dd398b883008060a8
marshall-strong <marshallstrong123@gmail.com>
2022-02-20 11:24:25 -0500

    add `2-BinaryNumberInALinkedList` starter code from HackerRank

---
+++ appAcademy_2022-02-14/2-BinaryNumberInALinkedList.js
@@ -0,0 +1,20 @@
+/*
+ * Complete the 'getNumber' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts INTEGER_SINGLY_LINKED_LIST binary as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+function getNumber(binary) {
+  // Write your code here
+}
