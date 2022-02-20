cffef8f3668407f114b2153627817c2ee014dc05
marshall-strong <marshallstrong123@gmail.com>
2022-02-20 15:50:07 -0500

    add my solution for the BinaryNumberInALinkedList `getNumber` function -- have not yet confirmed it works

--- appAcademy_2022-02-14/2-BinaryNumberInALinkedList.js
+++ appAcademy_2022-02-14/2-BinaryNumberInALinkedList.js
@@ -15,6 +15,16 @@
  *
  */
 
+// `binary`: reference to the head of a singly-linked list of binary digits
 function getNumber(binary) {
-  // Write your code here
+  // initialize the base 10 result to zero
+  let res = 0;
+  // traverse the linked list
+  while (binary != null) {
+    // multiply the current value of `res` by 2, then add `binary.data` for the current node
+    res = (res * 2) + (binary.data ? 1 : 0);
+    // move on to the next node
+    binary = binary.next;
+  }
+  return res;
 }
