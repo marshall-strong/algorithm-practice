245cb2e18b500540c121edb587bc48cfc8db9066
marshall-strong <marshallstrong123@gmail.com>
2022-02-20 11:25:05 -0500

    add `3-ThrottlingGateway` starter code from HackerRank

---
+++ appAcademy_2022-02-14/3-ThrottlingGateway.js
@@ -0,0 +1,10 @@
+/*
+ * Complete the 'droppedRequests' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY requestTime as parameter.
+ */
+
+function droppedRequests(requestTime) {
+  // Write your code here
+}
