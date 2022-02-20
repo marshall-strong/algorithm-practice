1673857e6234920af1e31c12e7d22dde85953ee6
marshall-strong <marshallstrong123@gmail.com>
2022-02-20 10:05:07 -0500

    add a "one line" solution for `fizzBuzz`

--- SampleTest/FizzBuzz.js
+++ SampleTest/FizzBuzz.js
@@ -5,7 +5,7 @@
  */
 
 // my solution
-function fizzBuzz(n) {
+function fizzBuzz0(n) {
   for (let i = 1; i <= n; i++) {
     if (i % 3 === 0) {
       if (i % 5 === 0) {
@@ -25,11 +25,17 @@ function fizzBuzz(n) {
 
 // https://flexiple.com/fizzbuzz-javascript/
 // most readable solution
-function fizzBuzz(n) {
+function fizzBuzz1(n) {
   for (let i = 1; i <= n; i++) {
     if (i % 15 == 0) console.log("FizzBuzz");
     else if (i % 3 == 0) console.log("Fizz");
     else if (i % 5 == 0) console.log("Buzz");
     else console.log(i);
   }
+}
+
+// https://flexiple.com/fizzbuzz-javascript/
+// one line solution
+function fizzBuzz2(n) {
+  for (let i = 1; i <= n;) console.log((i++ % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i);
 }
