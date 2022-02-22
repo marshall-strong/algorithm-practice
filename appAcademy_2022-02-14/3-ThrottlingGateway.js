9c680d9545d78049f28965abef7dc0e3ea4b1d37
marshall-strong <marshallstrong123@gmail.com>
2022-02-21 19:23:24 -0500

    add my solution for the ThrottlingGateway `droppedRequests` function -- have confirmed it works with the first example in the prompt

--- appAcademy_2022-02-14/3-ThrottlingGateway.js
+++ appAcademy_2022-02-14/3-ThrottlingGateway.js
@@ -6,5 +6,43 @@
  */
 
 function droppedRequests(requestTime) {
-  // Write your code here
+  let totalDropped = 0;
+
+  // `alreadyDropped` object tracks requests that have already been dropped
+  // object keys are array indices, object values are array elements
+  let alreadyDropped = {};
+
+  // alreadyDropped helper functions accept a requestTime index as a parameter
+  function logDroppedRequest(i) {
+    alreadyDropped[i] = requestTime[i];
+    totalDropped++;
+  } 
+  function checkIfAlreadyDropped(i) {
+    if (!alreadyDropped[i]) logDroppedRequest(i);
+  }
+
+  // gateway limiting helper functions accept a requestTime index as a parameter
+  function checkOneSecondLimit(i) {
+    if (requestTime[i] == requestTime[i - 3]) checkIfAlreadyDropped(i);
+  }
+  function checkTenSecondLimit(i) {
+    if (requestTime[i] - requestTime[i - 20] < 10) checkIfAlreadyDropped(i);
+  }
+  function checkOneMinuteLimit(i) {
+    if (requestTime[i] - requestTime[i - 60] < 60) checkIfAlreadyDropped(i);
+  }
+
+  const n = requestTime.length;
+
+  for (let i = 0; i < n; i++) {
+    if (i > 2) checkOneSecondLimit(i);
+    if (i > 19) checkTenSecondLimit(i);
+    if (i > 59) checkOneMinuteLimit(i);
+  }
+
+  return totalDropped;
 }
+
+// https://leetcode.com/discuss/interview-question/819577/Throttling-Gateway-Hackerrank
+
+const requestTimeExample = [ 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 7, 11, 11, 11, 11 ];
